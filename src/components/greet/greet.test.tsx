f1d115eb50b7c69a3a589312a6859fd2e963c012
Tahmid Tanzim <ttanzimlupin@presto.com>
2023-02-20 11:49:43 -0600

    Grouping Test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import {render, screen} from "@testing-library/react";
 import {Greet} from "./greet";
 
-test('Greet Render Correctly', () => {
-    render(<Greet/>);
-    const textElement = screen.getByText(/hello world/i);
-    expect(textElement).toBeInTheDocument();
-});
+describe('Greet', () => {
+    test('Render Correctly', () => {
+        render(<Greet/>);
+        const textElement = screen.getByText(/hello world/i);
+        expect(textElement).toBeInTheDocument();
+    });
 
-test('Greet Render with a name props', () => {
-    render(<Greet name='Tanzim'/>);
-    const textElement = screen.getByText(/hello world tanzim/i);
-    expect(textElement).toBeInTheDocument();
+    test('Render with a name props', () => {
+        render(<Greet name='Tanzim'/>);
+        const textElement = screen.getByText(/hello world tanzim/i);
+        expect(textElement).toBeInTheDocument();
+    });
 });
